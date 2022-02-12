269bf4510696c618fcd05eccf9f47d1431b6a0a8
Mateusz <resumovie@gmail.com>
2022-02-12 17:08:19 +0100

    Initial commit

---
+++ src/main/java/com/hrproject/HelloController.java
@@ -0,0 +1,12 @@
+package com.hrproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Welcome from index";
+    }
+}
